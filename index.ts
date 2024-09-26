808f91f3aadc17596d492d4693726fda11683db9
baetens.jan.hotmail <baetens.jan.hotmail@gmail.com>
2024-09-26 14:17:49 +0000

    first commit

---
+++ index.ts
@@ -0,0 +1,15 @@
+import 'dotenv/config'
+import axios from 'axios';
+import { HttpsProxyAgent } from 'https-proxy-agent';
+
+const tunnelTimeout = 10000; // 10 seconds
+
+async function main() {
+console.log("Test");
+}
+
+async function proxiedRequest(endpoint, headers, params) {
+
+}
+
+main();
