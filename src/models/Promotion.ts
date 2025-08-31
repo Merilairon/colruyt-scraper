1bc36125edee423fd2a8019ce47f6008cae6deae
Jan Baetens <baetens-jan@hotmail.com>
2025-08-31 11:26:02 +0200

    Adding top promo

--- src/models/Promotion.ts
+++ src/models/Promotion.ts
@@ -1,52 +1,53 @@
-import {DataTypes, Model} from "sequelize";
-import {sequelize} from "../database";
-import {Product} from "./Product";
-import {PromotionProduct} from "./PromotionProduct";
-import {Benefit} from "./Benefit";
+import { DataTypes, Model } from "sequelize";
+import { sequelize } from "../database";
+import { Product } from "./Product";
+import { PromotionProduct } from "./PromotionProduct";
+import { Benefit } from "./Benefit";
 
 export class Promotion extends Model {
-    declare promotionId: string;
-    declare promotionType: string;
-    declare activeStartDate: Date;
-    declare activeEndDate: Date;
-    declare seoBrandList: string[];
-    declare linkedTechnicalArticleNumber: string;
-    declare linkedCommercialArticleNumber: string;
-    declare products: Product[];
-    declare benefits: Benefit[];
+  declare promotionId: string;
+  declare promotionType: string;
+  declare activeStartDate: Date;
+  declare activeEndDate: Date;
+  declare topPromo: boolean;
+  declare seoBrandList: string[];
+  declare linkedTechnicalArticleNumber: string;
+  declare linkedCommercialArticleNumber: string;
+  declare products: Product[];
+  declare benefits: Benefit[];
 }
 
 Promotion.init(
-    {
-        promotionId: {
-            type: DataTypes.STRING,
-            primaryKey: true,
-        },
-        promotionType: DataTypes.STRING,
-        activeStartDate: DataTypes.DATEONLY,
-        activeEndDate: DataTypes.DATEONLY,
-        //benefit: DataTypes.JSON,
-        seoBrandList: DataTypes.JSON,
-        linkedTechnicalArticleNumber: DataTypes.JSON,
-        linkedCommercialArticleNumber: DataTypes.JSON,
+  {
+    promotionId: {
+      type: DataTypes.STRING,
+      primaryKey: true,
     },
-    {sequelize, modelName: "promotion"}
+    promotionType: DataTypes.STRING,
+    activeStartDate: DataTypes.DATEONLY,
+    activeEndDate: DataTypes.DATEONLY,
+    topPromo: DataTypes.BOOLEAN,
+    seoBrandList: DataTypes.JSON,
+    linkedTechnicalArticleNumber: DataTypes.JSON,
+    linkedCommercialArticleNumber: DataTypes.JSON,
+  },
+  { sequelize, modelName: "promotion" }
 );
 
 Promotion.belongsToMany(Product, {
-    through: PromotionProduct,
-    foreignKey: "promotionId",
+  through: PromotionProduct,
+  foreignKey: "promotionId",
 });
 
 Product.belongsToMany(Promotion, {
-    through: PromotionProduct,
-    foreignKey: "productId",
+  through: PromotionProduct,
+  foreignKey: "productId",
 });
 
 PromotionProduct.belongsTo(Promotion, {
-    foreignKey: "promotionId",
+  foreignKey: "promotionId",
 });
 
 PromotionProduct.belongsTo(Product, {
-    foreignKey: "productId",
+  foreignKey: "productId",
 });
