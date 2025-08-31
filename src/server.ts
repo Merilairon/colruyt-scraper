d617b5e17e307cb90e812a6b4b6cfc6c503e143f
Jan Baetens <baetens-jan@hotmail.com>
2025-08-31 11:58:45 +0200

    remove corse

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,6 @@ import productRoute from "./routes/products";
 import PromotionRoute from "./routes/promotions";
 import swaggerJsdoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
-import cors from "cors";
 import cron from "node-cron";
 
 import { scraper } from "./scraper";
@@ -20,8 +19,6 @@ app.use(
 );
 app.use(bodyParser.json());
 const port = process.env.PORT || 3000;
-
-app.use(cors());
 
 app.use("/api/products", productRoute);
 app.use("/api/promotions", PromotionRoute);
